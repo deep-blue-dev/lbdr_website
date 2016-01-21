2696bc3b5e3856bdf93f4ee8908f6a7fe5bebc07
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-20 21:18:10 -0500

    Generated Pages Controller for Group, Investor, Contact

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def group
+  end
+
+  def contact
+  end
+
+  def investor
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/group'
+
+  get 'pages/contact'
+
+  get 'pages/investor'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
