8be777b5be65f63d61ab6edbe44d69b9127d0c3a
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-24 19:32:24 -0500

    Uppercased Config variables...

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,10 +1,10 @@
 require "refile/s3"
 
 aws = {
-  access_key_id: "AWSAccessKeyId",
-  secret_access_key: "AWSSecretKey",
+  access_key_id: "AWS_ACCESS_KEY_ID",
+  secret_access_key: "AWS_SECRET_KEY",
   region: "us-east-1",
-  bucket: "AWSBucket",
+  bucket: "AWS_BUCKET_NAME",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
