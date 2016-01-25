81c4ce9c4b0ab1b9d234747f7cd2a8f0a91e9090
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-24 19:27:38 -0500

    Bc we live in the US. duh.

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,7 +3,7 @@ require "refile/s3"
 aws = {
   access_key_id: "AWSAccessKeyId",
   secret_access_key: "AWSSecretKey",
-  region: "sa-east-1",
+  region: "us-east-1",
   bucket: "AWSBucket",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
