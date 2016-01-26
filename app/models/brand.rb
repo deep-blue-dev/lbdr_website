2a3d01c6d3086cf5a787e2d6afc9640ce8cad785
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-26 10:53:16 -0500

    Remove secrets.yml file & add to git ignore. Also update brand model for s3 storage

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,16 +3,15 @@ class Brand < ApplicationRecord
 
   # Brand Photo File Uploads
   has_attached_file :brand_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
-    :download,
-    :storage => :s3,
-    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+                    :download,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :brand_photo, content_type: /\Aimage\/.*\Z/
 
   # Brand Logo File Uploads
   has_attached_file :logo_for_brand, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
-    :download,
-    :storage => :s3,
-    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :logo_for_brand, content_type: /\Aimage\/.*\Z/
 
   #  S3 credentials
