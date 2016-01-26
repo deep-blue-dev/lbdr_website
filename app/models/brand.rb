2ceec2a4aaccaa40453de8ef78f718d995fa44d2
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-25 23:05:47 -0500

    Took something out of brand model, stopped working. Putting back in.

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ApplicationRecord
   # belongs_to :sector
-  has_attached_file :brand_photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :brand_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :brand_photo, content_type: /\Aimage\/.*\Z/
-  has_attached_file :logo_for_brand, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :logo_for_brand, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :logo_for_brand, content_type: /\Aimage\/.*\Z/
 end
