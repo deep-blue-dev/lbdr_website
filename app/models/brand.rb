6363ac20638956d49d1a788224e9296fe234a9f0
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-25 21:43:02 -0500

    Change files:  brand_logo=>logo_for_brand and brand_image=>brand_photo

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ApplicationRecord
   # belongs_to :sector
-  has_attached_file :brand_image, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :brand_image, content_type: /\Aimage\/.*\Z/
-  has_attached_file :brand_logo, styles: { medium: "300x300>", thumb: "100x100>" }
-  validates_attachment_content_type :brand_logo, content_type: /\Aimage\/.*\Z/
+  has_attached_file :brand_photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :brand_photo, content_type: /\Aimage\/.*\Z/
+  has_attached_file :logo_for_brand, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :logo_for_brand, content_type: /\Aimage\/.*\Z/
 end
