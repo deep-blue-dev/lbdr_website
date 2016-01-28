495aa333eff2850c2d74a653551047da98300ceb
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-27 22:48:12 -0500

    Sector photo.

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,5 +1,7 @@
 class Sector < ApplicationRecord
   # has_many :brands
-  has_attached_file :sector_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :sector_photo, content_type: /\Aimage\/.*\Z/
+  has_attached_file :sector_photo, styles: { big: "1920x450#", thumb: "200x150>" },
+    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :sector_photo,
+    content_type: /\Aimage\/.*\Z/
 end
