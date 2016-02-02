18dd27d19c9c5f3da660ecde2e95b4480595bbf8
Kyra Fillmore <kfillmore27@gmail.com>
2016-02-01 19:09:30 -0500

    Created migration for social media

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127085054) do
+ActiveRecord::Schema.define(version: 20160202000826) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 20160127085054) do
     t.string   "sector_photo_content_type"
     t.integer  "sector_photo_file_size"
     t.datetime "sector_photo_updated_at"
+  end
+
+  create_table "social_media", force: :cascade do |t|
+    t.integer  "brand_id"
+    t.string   "type"
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_social_media_on_brand_id"
   end
 
   create_table "users", force: :cascade do |t|
