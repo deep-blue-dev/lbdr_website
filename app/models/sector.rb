6a562b5add2218853dc0059493eadd7074992314
Kyra Fillmore <kfillmore27@gmail.com>
2016-02-08 14:20:45 -0500

    images & hopefully a fix for sector thumbnails

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,8 +1,8 @@
 class Sector < ApplicationRecord
   has_many :brands
 
-  has_attached_file :sector_photo, styles: { big: "1920x450#", thumb: "200x150>" },
-    default_url: "/images/:style/missing.png"
+  has_attached_file :sector_photo, styles: { big: "1920x550#", thumb: "200x112#" },
+    default_url: "/images/:style/missing.jpg"
   validates_attachment_content_type :sector_photo,
     content_type: /\Aimage\/.*\Z/
 end
