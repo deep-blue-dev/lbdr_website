a616c1f93502c2250a4f32a336df1c04e42b4fbd
Kyra Fillmore <kfillmore27@gmail.com>
2016-02-17 10:42:01 -0500

    brand images

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,14 +3,14 @@ class Brand < ApplicationRecord
 
 # Brand Photo File Uploads
   has_attached_file :brand_photo,
-    styles: { big: "1920x500#", medium: "300x250>", thumb: "200x150#" },
+    styles: { big: "1920x500#"},
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :brand_photo,
     content_type: /\Aimage\/.*\Z/
 
   # Brand Logo File Uploads
   has_attached_file :logo_for_brand,
-    styles: { thumb: "200x150>" },
+    styles: { thumb: "200x150>", medium: "400x300>" },
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :logo_for_brand,
     content_type: /\Aimage\/.*\Z/
