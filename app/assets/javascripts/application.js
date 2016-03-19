d3d5dcd35ecaae38794aede3172dcc78d62c1e9f
Kyra Fillmore <kfillmore27@gmail.com>
2016-03-18 21:19:13 -0400

    Remove images and add placeholder images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,7 +46,7 @@ $(function() {
 $(function() {
   $(window).scroll( function(){
     $('.fadeInBlock').each( function(i){
-      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
       bottom_of_window = bottom_of_window + 170;
