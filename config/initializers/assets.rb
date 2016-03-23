aa6e18c7551e1b907faff7671c6a21ec06f3c670
Kyra Fillmore <kfillmore27@gmail.com>
2016-03-23 13:39:43 -0400

    Add new logos!

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w(lbdr-logo-white.png)
+Rails.application.config.assets.precompile += %w(LBDR_Emblem_White.svg LBDR_Simple_White.svg)
